1aca297823fc32fc0abafdac5f76b5e0188d833f
mushaid01 <67502604+mushaid01@users.noreply.github.com>
2022-06-01 00:32:44 -0700

    Update settings.py

--- assignment/django_auth/userprofile/userprofile/settings.py
+++ assignment/django_auth/userprofile/userprofile/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'userprofile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CRUD',
+        'USER':'postgres',
+        'PASSWORD':'3328',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
